ac6e4893a805cf6b2feff0b5a3ac677773488d00
Ethan <ethan1randolph@gmail.com>
2025-08-14 16:02:53 -0700

    Visual Studio extension for terraform failed to realize that I used an invalid type. Corrected the types for sg.

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -43,14 +43,14 @@ resource "aws_security_group" "sg" {
 
   egress {
     protocol    = "tcp"
-    cidr_blocks = "172.20.0.0/16"
+    cidr_blocks = ["172.20.0.0/16"]
     from_port   = 1024
     to_port     = 65535
   }
 
   ingress {
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
     from_port   = 80
     to_port     = 80
   }
